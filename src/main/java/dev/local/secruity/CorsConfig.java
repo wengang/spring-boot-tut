fbc502d49d1782d9f629703b7560eb50b1c028e5
wengang <tao-yu@hotmail.com>
2017-09-22 20:27:32 +0800

    change cors url

--- src/main/java/dev/local/secruity/CorsConfig.java
+++ src/main/java/dev/local/secruity/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("http://localhost:8082");
         config.addAllowedOrigin("https://www.getpostman.com");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
